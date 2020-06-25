3cdc8dec455e341ddbb5cd49b564d70bb5c830e6
Vincent Ho <hclvclho@gmail.com>
2020-06-25 22:16:53 +0800

    feat(book-promotion-list): book model

--- src/resolvers/bookPromotionList.js
+++ src/resolvers/bookPromotionList.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 import { BookPromotionListSchema } from '../models/BookPromotionList.js';
+import { BookSchema } from '../models/Book.js';
 const BookPromotionList = mongoose.model(
   'bookPromotionList',
   BookPromotionListSchema,
   'bookPromotionList',
 );
+const Book = mongoose.model('book', BookSchema);
 
 export default {
   Query: {
@@ -14,9 +16,10 @@ export default {
   },
   Mutation: {
     addPromotionBook: async (parent, { key, bookInfo }, { models }) => {
+      let book = new Book(bookInfo);
       await BookPromotionList.findOneAndUpdate(
         { key },
-        { $push: { books: bookInfo } },
+        { $push: { books: book } },
       );
       return bookInfo;
     },
