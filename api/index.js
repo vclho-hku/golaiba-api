9d8aeaf2656584e2ba194d0b93872a203466e81f
Vincent Ho <hclvclho@gmail.com>
2020-06-25 15:40:47 +0800

    feat(db): connect to mongodb

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-var admin = require('firebase-admin');
-const router = express.Router()
-
-router.get('/', (req, res) => {
-  res.status(200).json({"a": "abc"})
-})
-
-module.exports = router;
